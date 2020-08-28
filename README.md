a3618e26eef426c7f1143884fda24d97cb313804
DonatasD <donatasdaubar@gmail.com>
2020-08-29 01:00:20 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Danske technical assignment
+Technical assignment for Danske bank to determine holdem poker hand winner.
---
+++ src/main/java/com/donatasd/PokerHandEvaluator.java
@@ -0,0 +1,12 @@
+package com.donatasd;
+
+/**
+ * @author Donatas Daubaras
+ */
+public class PokerHandEvaluator {
+
+  public static void main(String[] args) {
+
+  }
+
+}
